56b7f85986376e4fbf4937322d3b301a32e271d7
Kieran132 <kiemclean@hotmail.co.uk>
2022-11-10 15:08:35 +0000

    Add function to obtain question and options from array

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,8 @@ const info = document.querySelector(".info");
 const continue_btn = info.querySelector(".buttons .continue");
 const quiz = document.querySelector(".quiz");
 
+const option_area = document.querySelector(".option_area")
+
 //When start button is clicked
 start_btn.onclick = () => {
     info.classList.add("activeInfo");
@@ -15,6 +17,7 @@ continue_btn.onclick = () => {
     start_btn.classList.add("unactive")
     info.classList.remove("activeInfo")
     quiz.classList.add("activeQuiz")
+    showQuestions(0);
 }
 
 //When next button is clicked
@@ -22,3 +25,17 @@ continue_btn.onclick = () => {
 // When replay button is clicked
 
 //Getting questions and options from array
+function showQuestions(index){
+    const question_area = document.querySelector(".question_area");
+    let que_tag = '<span>' + questions[index].numb + '.' + questions[index].question + '</span>';
+    let option_tag = '<div class="option">'+ questions[index].options[0] + '<span></span></div>'
+                    +'<div class="option">'+ questions[index].options[1] + '<span></span></div>'
+                    +'<div class="option">'+ questions[index].options[2] + '<span></span></div>'
+                    +'<div class="option">'+ questions[index].options[3] + '<span></span></div>';
+    question_area.innerHTML = que_tag;
+    option_area.innerHTML = option_tag;
+    const option = option_area.querySelectorAll(".option");
+    for (let i = 0; i < option.length; i++){
+        option[i].setAttribute("onclick", "optionSelected(this)");
+    }
+}
