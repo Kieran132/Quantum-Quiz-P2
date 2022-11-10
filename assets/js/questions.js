a031a24439cee9e035ac6e8a2cb139f987ba5da7
Kieran132 <kiemclean@hotmail.co.uk>
2022-11-10 13:59:47 +0000

    Add questions array to questios.js

---
+++ assets/js/questions.js
@@ -0,0 +1,70 @@
+//creating an array and passing the number , questions, options and answers
+
+let questions = [
+    {
+        numb: 1,
+        question: 'How many moons does Saturn have',
+        answer: '83',
+        options:[
+            '4',
+            '100',
+            '83',
+            '72'
+        ]
+    },
+    {
+        numb: 2,
+        question: 'Which planet is the biggest in our solar system?',
+        answer: 'Jupiter',
+        options:[
+            'Pluto',
+            'Mars',
+            'Venus',
+            'Jupiter'
+        ]
+    },
+    {
+        numb: 3,
+        question: 'In order of their distances from the Sun, which of the following planets lie between Mars and Uranus??',
+        answer: 'Jupiter and Saturn',
+        options:[
+            'Earth and Jupiter',
+            'Jupiter and Saturn',
+            'Saturn and Earth',
+            'Saturn and Neptune'
+        ]
+    },
+    {
+        numb: 4,
+        question: 'Which planet is known as the "Watery Planet" ?',
+        answer: 'Earth',
+        options:[
+            'Marcury',
+            'Earth',
+            'Mars',
+            'Jupiter'
+        ]
+    },
+    {
+        numb: 5,
+        question: 'Which of the following is indicated by the colour of a star?',
+        answer: 'Temperature',
+        options:[
+            'Weight',
+            'Distance',
+            'Temperature',
+            'Size'
+        ]
+    },
+    {
+        numb: 6,
+        question: 'Milky Way Galaxy was first seen by ?',
+        answer: 'Galileo',
+        options:[
+            'Galileo',
+            'Newton',
+            'Marconi',
+            'Martin Schmidt'
+        ]
+    },
+]
