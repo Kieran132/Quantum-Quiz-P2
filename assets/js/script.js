3a9b22a1a50b71cf017e7355206248f7dc596f48
Kieran132 <kiemclean@hotmail.co.uk>
2022-11-17 13:57:03 +0000

    formatted script.js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,20 @@
 // Getting all the required elements
 
-const home_page = document.querySelector(".home_page")
+const home_page = document.querySelector(".home_page");
 const start_btn = document.querySelector(".start_btn button");
 const info = document.querySelector(".info");
 const continue_btn = info.querySelector(".buttons .continue");
 const quiz = document.querySelector(".quiz");
 
-const option_area = document.querySelector(".option_area")
-const next_btn = quiz.querySelector(".next_btn")
-const results = document.querySelector(".results")
+const option_area = document.querySelector(".option_area");
+const next_btn = quiz.querySelector(".next_btn");
+const results = document.querySelector(".results");
 
-const quit = results.querySelector(".buttons_end .stop")
+const quit = results.querySelector(".buttons_end .stop");
 
-const username = document.querySelector(".username_inupt")
-const submit = document.querySelector(".submit")
-const storedUsername = localStorage.getItem("text")
+const username = document.querySelector(".username_inupt");
+const submit = document.querySelector(".submit");
+const storedUsername = localStorage.getItem("text");
 
 let que_count = 0;
 let que_number = 1;
@@ -25,34 +25,34 @@ let userScore = 0;
 start_btn.onclick = () => {
     info.classList.add("activeInfo");
     home_page.classList.add("unactive");
-}
+};
 
 //When continue button is clicked
 continue_btn.onclick = () => {
-    home_page.classList.add("unactive")
-    info.classList.remove("activeInfo")
-    quiz.classList.add("activeQuiz")
+    home_page.classList.add("unactive");
+    info.classList.remove("activeInfo");
+    quiz.classList.add("activeQuiz");
     showQuestions(0);
     questionCounter(1);
-}
+};
 
 //When next button is clicked
 next_btn.onclick = () =>{
     if(que_count < questions.length - 1){
         que_count++;
          que_number++;
-        showQuestions(que_count)
+        showQuestions(que_count);
         questionCounter(que_number);
     } else {
         console.log("Questions ended");
         showResults();
     }
-}
+};
 
 // When exit button is clicked
 quit.onclick = () =>{
     window.location.reload();
-}
+};
 
 
 
@@ -61,9 +61,9 @@ function showQuestions(index){
     const question_area = document.querySelector(".question_area");
     let que_tag = '<span>' + questions[index].numb + '.' + questions[index].question + '</span>';
     let option_tag = '<div class="option">'+ questions[index].options[0] + '<span></span></div>'
-                    +'<div class="option">'+ questions[index].options[1] + '<span></span></div>'
-                    +'<div class="option">'+ questions[index].options[2] + '<span></span></div>'
-                    +'<div class="option">'+ questions[index].options[3] + '<span></span></div>';
+                    + '<div class="option">' + questions[index].options[1] + '<span></span></div>'
+                    + '<div class="option">' + questions[index].options[2] + '<span></span></div>'
+                    + '<div class="option">' + questions[index].options[3] + '<span></span></div>';
     question_area.innerHTML = que_tag;
     option_area.innerHTML = option_tag;
     const option = option_area.querySelectorAll(".option");
@@ -72,7 +72,7 @@ function showQuestions(index){
     }
 }
 
-//Function for when user chooses and answer
+//Function for when user chooses an answer
 
 function optionSelected(answer){
     let userAnswer = answer.textContent;
@@ -104,30 +104,30 @@ function optionSelected(answer){
 
 function questionCounter(index){
     const bottom_ques_count = quiz.querySelector(".question_count");
-    let totalQuesCount =  '<span>'+ index + '<p>of</p>' + questions.length + '<p>Questions</p></span>'
+    let totalQuesCount =  '<span>'+ index + '<p>of</p>' + questions.length + '<p>Questions</p></span>';
     bottom_ques_count.innerHTML = totalQuesCount;
 }
 
 // Results box
 
 function showResults(){
-    start_btn.classList.add("unactive")
-    info.classList.remove("activeInfo")
-    quiz.classList.remove("activeQuiz")
-    results.classList.add("activeResults")
-    const score_info = results.querySelector(".score_text")
+    start_btn.classList.add("unactive");
+    info.classList.remove("activeInfo");
+    quiz.classList.remove("activeQuiz");
+    results.classList.add("activeResults");
+    const score_info = results.querySelector(".score_text");
     if(userScore > 4){
-        let scoreMsg = '<span>Well Done, <p>' + storedUsername + '!</p>' + userScore + '</p>out of<p>' + questions.length + '</p></span>'
-        score_info.innerHTML = scoreMsg
+        let scoreMsg = '<span>Well Done, <p>' + storedUsername + '!</p>' + userScore + '</p>out of<p>' + questions.length + '</p></span>';
+        score_info.innerHTML = scoreMsg;
     } else if (userScore > 2){
-        let scoreMsg = '<span>Not Bad, Not Good <p>' + userScore + '</p>out of<p>' + questions.length + '</p></span>'
-        score_info.innerHTML = scoreMsg
+        let scoreMsg = '<span>Not Bad, Not Good <p>' + userScore + '</p>out of<p>' + questions.length + '</p></span>';
+        score_info.innerHTML = scoreMsg;
     } else {
-        let scoreMsg = '<span>Back To School For You <p>' + storedUsername + '!</p>' + userScore + '</p>out of<p>' + questions.length + '</p></span>'
-        score_info.innerHTML = scoreMsg
+        let scoreMsg = '<span>Back To School For You <p>' + storedUsername + '!</p>' + userScore + '</p>out of<p>' + questions.length + '</p></span>';
+        score_info.innerHTML = scoreMsg;
     }
 }
 
 submit.onclick = () => {
     localStorage.setItem("text", username.value);
-}
+};
