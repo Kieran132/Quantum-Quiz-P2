6d5272ad00d9362a29a31debffbdd793246f6901
Kieran132 <kiemclean@hotmail.co.uk>
2022-12-05 19:46:15 +0000

    Amended message in the results

--- assets/js/script.js
+++ assets/js/script.js
@@ -110,13 +110,13 @@ function showResults(){
     results.classList.add("activeResults");
     const score_info = results.querySelector(".score_text");
     if(userScore > 4){
-        let scoreMsg = '<span>Well Done, <p>' + storedUsername + '!</p>' + userScore + '</p>out of<p>' + questions.length + '</p></span>';
+        let scoreMsg = '<span>Well Done, <p>' + storedUsername + userScore + '</p>out of<p>' + questions.length + '</p></span>';
         score_info.innerHTML = scoreMsg;
     } else if (userScore > 2){
-        let scoreMsg = '<span>Not Bad, Not Good <p>' + userScore + '</p>out of<p>' + questions.length + '</p></span>';
+        let scoreMsg = '<span>Not Bad, Not Good <p>' + storedUsername + userScore + '</p>out of<p>' + questions.length + '</p></span>';
         score_info.innerHTML = scoreMsg;
     } else {
-        let scoreMsg = '<span>Back To School For You <p>' + storedUsername + '!</p>' + userScore + '</p>out of<p>' + questions.length + '</p></span>';
+        let scoreMsg = '<span>Back To School For You <p>' + storedUsername + userScore + '</p>out of<p>' + questions.length + '</p></span>';
         score_info.innerHTML = scoreMsg;
     }
 }
